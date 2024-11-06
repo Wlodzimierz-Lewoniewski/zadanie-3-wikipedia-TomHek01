aa9664691977b53fc050647564c5df37e8a45206
Tomasz Olender <83708@student.ue.poznan.pl>
2024-11-06 14:39:33 +0100

    Update6.11

--- main.py
+++ main.py
@@ -1,68 +1,79 @@
-import re
 import requests
+from bs4 import BeautifulSoup
+import html
 
-def fetch_html(url):
-    """Pobiera kod HTML strony pod danym URL."""
+
+# Funkcja do pobierania linków z kategorii
+def get_category_links(category):
+    url = f'https://pl.wikipedia.org/wiki/Kategoria:{category}'
     response = requests.get(url)
+
     if response.status_code == 200:
-        return response.text
-    return ""
+        soup = BeautifulSoup(response.text, 'html.parser')
+        section = soup.find("div", class_="mw-category mw-category-columns")
+        return [a['href'] for a in section.find_all("a")][:2]
+    else:
+        print(f"Error: {response.status_code}")
+        return []
 
-def parse_article_page(html):
-    """Analizuje HTML artykułu i zwraca wymagane informacje."""
-    
-    # Wewnętrzne odnośniki do innych artykułów Wikipedii (pierwsze 5)
-    internal_links = re.findall(r'<a href="(/wiki/[^":#]+)"[^>]*>([^<]+)</a>', html)
-    internal_links = [link[1] for link in internal_links if ':' not in link[0]][:5]
-    
-    # URL-e obrazków (pierwsze 3), wyłączając nawigacyjne ikony
-    image_urls = re.findall(r'<img[^>]+src="(//upload\.wikimedia\.org[^"]+)"', html)
-    image_urls = ["https:" + url for url in image_urls[:3]]
-    
-    # URL-e zewnętrzne (pierwsze 3), z klasą "external"
-    external_links = re.findall(r'<a[^>]+href="(https?://[^"]+)"[^>]*class="external"', html)
-    external_links = external_links[:3]
-    
-    # Kategorie (pierwsze 3), wykluczając nawigacyjne elementy
-    categories = re.findall(r'<a[^>]+href="/wiki/Kategoria:[^"]+"[^>]*>([^<]+)</a>', html)
-    categories = categories[:3]
 
-    return {
-        "internal_links": internal_links or [""],
-        "image_urls": image_urls or [""],
-        "external_links": external_links or [""],
-        "categories": categories or [""]
-    }
+# Funkcja do pobierania danych artykułu
+def get_article_data(article_link):
+    url = f'https://pl.wikipedia.org{article_link}'
+    response = requests.get(url)
 
-def print_formatted_data(data):
-    """Formatuje i drukuje dane zgodnie z wymaganiami."""
-    
-    # Wewnętrzne odnośniki
-    print(" | ".join(data["internal_links"]))
-    
-    # URL-e obrazków
-    print(" | ".join(data["image_urls"]))
-    
-    # URL-e zewnętrzne
-    print(" | ".join(data["external_links"]))
-    
-    # Kategorie
-    print(" | ".join(data["categories"]))
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        content_div = soup.find("div", class_="mw-body-content")
 
+        # Internal links (first 5)
+        internal_links = [a['title'] for a in content_div.find_all('a', href=True)
+                          if a['href'].startswith('/wiki/') and ':' not in a['href'][6:]]
+        internal_links_summary = " | ".join(internal_links[:5])
+
+        # Image URLs (first 3)
+        images = [img['src'] for img in content_div.find_all('img') if '/wiki/' not in img['src']][:3]
+        images_summary = " | ".join(images) if images else ""
+
+        # External sources (first 3)
+        refs_div = soup.find("div", class_="mw-references-wrap mw-references-columns")
+        if refs_div is None:
+            refs_div = soup.find("div", class_="do-not-make-smaller refsection")
+
+        external_links = []
+        if refs_div:
+            ref_items = refs_div.find_all("li")
+            for item in ref_items[:3]:
+                ref_links = item.find_all("span", class_="reference-text")
+                for link in ref_links:
+                    external_links.extend([a['href'] for a in link.find_all("a", href=True) if "http" in a['href']])
+
+        external_links_summary = " | ".join([html.escape(link) for link in external_links[:3]])
+
+        # Categories
+        category_section = soup.find("div", class_="mw-normal-catlinks")
+        categories = [a.text.strip() for a in category_section.find_all("a")][:3] if category_section else []
+        categories_summary = " | ".join(categories)
+
+        # Return gathered data
+        return [internal_links_summary, images_summary, external_links_summary, categories_summary]
+
+    else:
+        print(f"Error: {response.status_code}")
+        return []
+
+
+# Funkcja główna
 def main():
-    # Odczytaj kategorię od użytkownika
-    category = input().strip()
-    category_url = f'https://pl.wikipedia.org/wiki/Kategoria:{category.replace(" ", "_")}'
-    
-    # Pobierz listę artykułów z kategorii
-    html = fetch_html(category_url)
-    article_links = re.findall(r'<a href="(/wiki/[^":#]+)"[^>]*>([^<]+)</a>', html)
-    article_urls = ['https://pl.wikipedia.org' + link[0] for link in article_links[:2]]
-    
-    # Przetwórz i wyświetl dane dla każdego artykułu
-    for article_url in article_urls:
-        article_html = fetch_html(article_url)
-        data = parse_article_page(article_html)
-        print_formatted_data(data)
+    category = input().replace(" ", "_")
 
-main()
+    links = get_category_links(category)
+    for link in links:
+        data = get_article_data(link)
+        for item in data:
+            print(item)
+
+
+# Uruchomienie programu
+if __name__ == "__main__":
+    main()
